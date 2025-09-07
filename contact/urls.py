e7443512469dfe76c2a7605401078bf2a9c24e48
Murilo <twitterfo131@gmail.com>
2025-09-07 15:50:00 -0300

    primeiro migrate + testes simples com view e templates de Feira

--- contact/urls.py
+++ contact/urls.py
@@ -6,4 +6,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('feira/<int:pk>', views.FeiraView.as_view(), name = 'feira'),
+    path('barraca/<int:pk>', views.BarracaView.as_view(), name = 'barraca'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Feira, Barraca
+from django.views import generic
+
 
 
 def index(request):
     return render(
         request, 'contact/index.html',
     )
+
+class FeiraView(generic.ListView):
+    model = Feira
+    template_name = "contact/feira_list.html"
+    paginate_by = 10
+
+class BarracaView(generic.DetailView):
+    model = Barraca
+    template_name = ""
+    
+
